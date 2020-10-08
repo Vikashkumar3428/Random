8ba0fd94b3ed9af844a17a0c7dfed4dfa974ef5d
Vikashkumar3428 <vkumar3428@gmail.com>
2020-10-08 12:20:25 +0530

    Updates

---
+++ precache-manifest.515aece22d4d624c825aa0396fc1a8da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7049f76419a0f7f371ebbdc3eacc7f4a",
+    "url": "/Random/index.html"
+  },
+  {
+    "revision": "6c8d29dc8ad0fff39e26",
+    "url": "/Random/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ef8f11f1ce458b7ea59",
+    "url": "/Random/static/js/2.60791b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random/static/js/2.60791b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8d29dc8ad0fff39e26",
+    "url": "/Random/static/js/main.b8ca238e.chunk.js"
+  },
+  {
+    "revision": "3672ac006a1d615bf4d3",
+    "url": "/Random/static/js/runtime-main.43a4ebcb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Random/static/media/logo.5d5d9eef.svg"
+  }
+]);
